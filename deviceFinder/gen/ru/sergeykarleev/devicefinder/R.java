09f9af683d686588882690611f872695bcd0bda9
sergeykarleev <sergey@shaman>
2014-12-12 23:25:54 +0300

    + Разметка экрана настроек - Запрограммировать SharedPreferences

--- deviceFinder/gen/ru/sergeykarleev/devicefinder/R.java
+++ deviceFinder/gen/ru/sergeykarleev/devicefinder/R.java
@@ -25,7 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int btnCancel=0x7f080006;
+        public static final int btnSave=0x7f080005;
+        public static final int editText4=0x7f080004;
+        public static final int etCodeCallback=0x7f080000;
+        public static final int etCodeGEO=0x7f080001;
+        public static final int etPhone1=0x7f080002;
+        public static final int etPhone2=0x7f080003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -34,9 +41,18 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int button_cancel=0x7f050009;
+        public static final int button_save=0x7f050008;
+        /**  Тексты экрана настроек 
+         */
+        public static final int title_code_callback=0x7f050002;
+        public static final int title_code_geo=0x7f050003;
+        public static final int title_phone_1=0x7f050004;
+        public static final int title_phone_2=0x7f050005;
+        public static final int title_phone_3=0x7f050006;
+        public static final int title_phone_help=0x7f050007;
     }
     public static final class style {
         /** 
--- deviceFinder/src/ru/sergeykarleev/devicefinder/SMSMonitor.java
+++ deviceFinder/src/ru/sergeykarleev/devicefinder/SMSMonitor.java
@@ -3,6 +3,7 @@ package ru.sergeykarleev.devicefinder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 
@@ -11,27 +12,29 @@ public class SMSMonitor extends BroadcastReceiver {
 	public final static String ACTION = "android.provider.Telephony.SMS_RECEIVED";
 	public final static String SMS_BODY = "sms_body";
 
+	SharedPreferences sPref;
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		
+		//sPref = context.getSharedPreferences(name, mode)
 		if (intent.getAction().equals(SMSMonitor.ACTION)) {
-			
+
 			StringBuilder sb = new StringBuilder();
 			Bundle bundle = intent.getExtras();
-			if (bundle!=null){
+			if (bundle != null) {
 				Object[] pdus = (Object[]) bundle.get("pdus");
 				for (Object pdu : pdus) {
-					SmsMessage message = SmsMessage.createFromPdu((byte[]) pdu);					
+					SmsMessage message = SmsMessage.createFromPdu((byte[]) pdu);
 					sb.append(message.getDisplayMessageBody());
 				}
-			}		
-			
-			Intent activityIntent = new Intent();			
-	        activityIntent.setClassName("ru.sergeykarleev.smstimer", "ru.sergeykarleev.smstimer.MainActivity");
-	        activityIntent.putExtra(SMS_BODY, sb.toString());
-	        activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-	        context.startActivity(activityIntent);
-		}		
+			}
 
+			Intent activityIntent = new Intent();
+			activityIntent.setClassName("ru.sergeykarleev.devicefinder",
+					"ru.sergeykarleev.devicefinder.MainActivity");
+			activityIntent.putExtra(SMS_BODY, sb.toString());
+			activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			context.startActivity(activityIntent);
+		}
 	}
 }
